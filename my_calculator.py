1da448da42989f1d51027a8f20675dcb8be039d0
Cyborgkong <adammoses322@gmail.com>
2024-08-17 18:38:17 +0100

    Add files via upload

---
+++ my_calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number"))
+operator = ("Select operator:","-+/*")
+num2 = int(input("Enter second number"))
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '/':
+    print(num1 / num2)
+elif operator == '*':
+    print(num1 * num2)
+else:
+    print("Syntax error. try again")
